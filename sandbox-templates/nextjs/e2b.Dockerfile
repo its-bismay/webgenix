652a56aeac28ab2ce154bc8af75990b3497f70c0
its-bismay <2023ugee070@nitjsr.ac.in>
2026-04-25 12:52:38 +0530

    remove sensitive inngest files

---
+++ sandbox-templates/nextjs/e2b.Dockerfile
@@ -0,0 +1,18 @@
+# You can use most Debian-based base images
+FROM node:21-slim
+
+# Install curl
+RUN apt-get update && apt-get install -y curl && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+COPY compile_page.sh /compile_page.sh
+RUN chmod +x /compile_page.sh
+
+# Install dependencies and customize sandbox
+WORKDIR /home/user/nextjs-app
+
+RUN npx --yes create-next-app@15.5.15 . --yes
+
+RUN npx shadcn@4.0.5 init --base base --preset nova --yes --force
+RUN npx --yes shadcn@4.0.5 add --all --yes
+# Move the Nextjs app to the home directory and remove the nextjs-app directory
+RUN mv /home/user/nextjs-app/* /home/user/ && rm -rf /home/user/nextjs-app
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { createAgent, gemini } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const processTask = inngest.createFunction(
 	{ id: "process-task" },
 	{ event: "app/task.created" },
-	async ({ event }) => {
+	async ({ event, step }) => {
+		const sandboxId = await step.run("get-sandbox-id", async () => {
+			const sandbox = await Sandbox.create("d5c9c0yzchplwomv2vrp");
+			return sandbox.sandboxId;
+		});
+
 		const agent = createAgent({
 			name: "ai-agent",
 			system: "You are an assistant for answering questions to user queries.",
@@ -15,6 +22,12 @@ export const processTask = inngest.createFunction(
 			`Answer the following user query: ${event.data.input}`,
 		);
 
-		return { output };
+		const sandboxUrl = await step.run("get-sandbox-url", async () => {
+			const sandbox = await getSandbox(sandboxId);
+			const host = sandbox.getHost(3000);
+			return `https://${host}`;
+		});
+
+		return { output, sandboxUrl };
 	},
 );
